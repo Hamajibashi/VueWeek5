a7b7d404da4c3defd7294ccc151314d8d416b9ba
Mylène <andreasakata@gmail.com>
2022-03-16 20:16:02 +0800

    安裝 ESLint

--- cart.js
+++ cart.js
@@ -2,5 +2,3 @@ import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.e
 
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
 const apiPath = 'mylmii';
-
-console.log(apiUrl, apiPath, '載入成功');
