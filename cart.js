37449aa88f71405bac42b598f3ae925ce9c33946
Mylène <andreasakata@gmail.com>
2022-03-16 18:25:51 +0800

    Initial commit

---
+++ cart.js
@@ -0,0 +1,6 @@
+import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.prod.min.js';
+
+const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
+const apiPath = 'mylmii';
+
+console.log(apiUrl, apiPath, '載入成功');
